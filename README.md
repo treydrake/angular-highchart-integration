cda80d95f1d3dafb89d84ca523ea894c839630de
treydrake <treydrake@yahoo.com>
2017-11-16 10:36:50 -0800

    add charting

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.0.
 
-## Development server
+## Developmentls
+ server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ChartComponent} from './chart/chart.component';
+import {ChartModule, HIGHCHARTS_MODULES} from "angular-highcharts";
+import exporting from 'highcharts/modules/exporting.src.js';
+import drilldown from 'highcharts/modules/drilldown.src.js';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {LocationStrategy, PathLocationStrategy} from "@angular/common";
+
+export function highchartsModules() {
+  return [exporting, drilldown];
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule,
+    AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: PathLocationStrategy},
+    {provide: HIGHCHARTS_MODULES, useFactory: highchartsModules},
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
